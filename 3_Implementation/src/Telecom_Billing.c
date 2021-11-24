7e66843f49a4657d43f5f69566d6edd2a900d6f0
Adwaith1999 <94242252+Adwaith1999@users.noreply.github.com>
2021-11-24 11:17:44 +0530

    Update Telecom_Billing.c

--- 3_Implementation/src/Telecom_Billing.c
+++ 3_Implementation/src/Telecom_Billing.c
@@ -14,7 +14,18 @@ int deleterecords(char phonenumber,char name,float amount)
 }
 int searchrecords(char phonenumber,char name,float amount)
 {
-}
+  if(strcmp(s.phonenumber,phonenumber)==0)
+		{	
+			printf( "Record Found");
+			flag=0;
+			break;
+		}
+		else if(flag==1)
+		{	
+			printf("Requested Phone Number Not found in our database");
+		}
+	}
+
 float payment(char phonenumber,char name,float amount)
 {
   amount = amount - rem amount;
